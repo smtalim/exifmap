f5dc399c5a67c609a97c4ef4abb403451ccd03b5
SatishTalim <satish@rubylearning.org>
2013-08-16 06:31:57 +0530

    commit

--- exifmap.rb
+++ exifmap.rb
@@ -11,13 +11,13 @@ begin
   
   lat = photo.gps.latitude.to_s
   lng = photo.gps.longitude.to_s
-  imgsrc = "http://maps.googleapis.com/maps/api/staticmap?sensor=false&zoom=5&size=600x300&maptype=roadmap&center=#{lat},#{lng}&markers=color:blue%7Clabel:I%7C#{lat},#{lng}"
-  p imgsrc
+  photo_map = "http://maps.googleapis.com/maps/api/staticmap?sensor=false&zoom=5&size=600x300&maptype=roadmap&center=#{lat},#{lng}&markers=color:blue%7Clabel:I%7C#{lat},#{lng}"
+  puts photo_map
 
 rescue NoMethodError
-  p "Sorry. Your image has no longitude and latitude info in the photo metadata." 
+  puts "Sorry. Your image has no longitude and latitude info in the photo metadata." 
 rescue Exception => e  
-  p e.message
+  puts e.message
 end
 
 
